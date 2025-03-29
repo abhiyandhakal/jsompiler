d7e1361316e07cc6d29d5b5d734265e516903baf
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-03-29 12:54:40 +0545

    refactor: modularie code to break down into separate crates

---
+++ crates/jsompiler_common/src/lib.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum ErrorKind {
+    LexerError,
+    SyntaxError,
+    SemanticError,
+    UnexpectedToken,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Error {
+    pub error_kind: ErrorKind,
+    pub message: String,
+    pub line_number: usize,
+    pub pos: usize,
+}
+
+impl Error {
+    pub fn new(error_kind: ErrorKind, message: String, line_number: usize, pos: usize) -> Self {
+        Self {
+            error_kind,
+            message,
+            line_number,
+            pos,
+        }
+    }
+}
