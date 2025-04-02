96b30600f48d91cf662b66480bafaca4542bb1b5
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-04-02 10:07:22 +0545

    feat(lexer): optimize string lexing code (make code dry)

--- crates/jsompiler_lexer/src/string.rs
+++ crates/jsompiler_lexer/src/string.rs
@@ -6,13 +6,22 @@ use crate::{
 };
 
 impl Lexer {
-    pub fn lex_backtick_string(&mut self) -> Result<Option<Lexeme>, Error> {
+    pub fn lex_string(&mut self, ch: char) -> Result<Option<Lexeme>, Error> {
         let mut processed_string = "".to_string();
-        while self.get_current_char() != '`' {
+        while self.get_current_char() != ch {
             if self.get_current_char() == '\0' {
                 return Err(Error {
                     error_kind: ErrorKind::LexerError,
-                    message: "Template string (`) not closed.".to_string(),
+                    message: format!("String ({ch}) not closed."),
+                    line_number: self.line_number,
+                    pos: self.current,
+                });
+            }
+
+            if ch != '`' && self.get_current_char() == '\n' {
+                return Err(Error {
+                    error_kind: ErrorKind::LexerError,
+                    message: format!("String ({ch}) not closed."),
                     line_number: self.line_number,
                     pos: self.current,
                 });
@@ -22,10 +31,6 @@ impl Lexer {
                 if self.current + 1 < self.source.len() {
                     let next_char = self.source[self.current + 1];
                     match next_char {
-                        '`' => {
-                            processed_string.push('`');
-                            self.advance(); // Skip `\`
-                        }
                         '\\' => {
                             processed_string.push('\\');
                             self.advance(); // Skip `\`
@@ -38,9 +43,14 @@ impl Lexer {
                             processed_string.push('\t');
                             self.advance(); // Skip `\`
                         }
-                        _ => {
-                            processed_string.push(next_char);
-                            self.advance(); // Skip `\`
+                        c => {
+                            if c == ch {
+                                processed_string.push(ch);
+                                self.advance(); // Skip ch
+                            } else {
+                                processed_string.push(next_char);
+                                self.advance(); // Skip `\`
+                            }
                         }
                     }
                 }
@@ -50,128 +60,14 @@ impl Lexer {
             self.advance();
         }
         self.advance(); // consume the closing quote
-        Ok(Some(lexeme(
-            self.source[self.start..self.current].iter().collect(),
-            Token::Literal(LiteralToken::String(symbol::StringLiteral::Template(
-                processed_string,
-            ))),
-        )))
-    }
-
-    pub fn lex_doublequote_string(&mut self) -> Result<Option<Lexeme>, Error> {
-        let mut processed_string = "".to_string();
-        while self.get_current_char() != '"' {
-            if self.get_current_char() == '\0' {
-                return Err(Error {
-                    error_kind: ErrorKind::LexerError,
-                    message: "String (\") not closed.".to_string(),
-                    line_number: self.line_number,
-                    pos: self.current,
-                });
-            }
-
-            if self.get_current_char() == '\n' {
-                return Err(Error {
-                    error_kind: ErrorKind::LexerError,
-                    message: "String (\") not closed.".to_string(),
-                    line_number: self.line_number,
-                    pos: self.current,
-                });
-            }
-
-            if self.get_current_char() == '\\' {
-                if self.current + 1 < self.source.len() {
-                    let next_char = self.source[self.current + 1];
-                    match next_char {
-                        '"' => {
-                            processed_string.push('"');
-                            self.advance(); // Skip `\`
-                        }
-                        '\\' => {
-                            processed_string.push('\\');
-                            self.advance(); // Skip `\`
-                        }
-                        'n' => {
-                            processed_string.push('\n');
-                            self.advance(); // Skip `\`
-                        }
-                        't' => {
-                            processed_string.push('\t');
-                            self.advance(); // Skip `\`
-                        }
-                        _ => {
-                            processed_string.push(next_char);
-                            self.advance(); // Skip `\`
-                        }
-                    }
-                }
-            } else {
-                processed_string.push(self.get_current_char());
-            }
-            self.advance();
+        if ch == '`' {
+            return Ok(Some(lexeme(
+                self.source[self.start..self.current].iter().collect(),
+                Token::Literal(LiteralToken::String(symbol::StringLiteral::Template(
+                    processed_string,
+                ))),
+            )));
         }
-        self.advance(); // consume the closing quote
-        Ok(Some(lexeme(
-            self.source[self.start..self.current].iter().collect(),
-            Token::Literal(LiteralToken::String(symbol::StringLiteral::Regular(
-                processed_string,
-            ))),
-        )))
-    }
-
-    pub fn lex_singlequote_string(&mut self) -> Result<Option<Lexeme>, Error> {
-        let mut processed_string = "".to_string();
-        while self.get_current_char() != '\'' {
-            if self.get_current_char() == '\0' {
-                return Err(Error {
-                    error_kind: ErrorKind::LexerError,
-                    message: "String (') not closed.".to_string(),
-                    line_number: self.line_number,
-                    pos: self.current,
-                });
-            }
-
-            if self.get_current_char() == '\n' {
-                return Err(Error {
-                    error_kind: ErrorKind::LexerError,
-                    message: "String (') not closed.".to_string(),
-                    line_number: self.line_number,
-                    pos: self.current,
-                });
-            }
-
-            if self.get_current_char() == '\\' {
-                if self.current + 1 < self.source.len() {
-                    let next_char = self.source[self.current + 1];
-                    match next_char {
-                        '\'' => {
-                            processed_string.push('\'');
-                            self.advance(); // Skip `\`
-                        }
-                        '\\' => {
-                            processed_string.push('\\');
-                            self.advance(); // Skip `\`
-                        }
-                        'n' => {
-                            processed_string.push('\n');
-                            self.advance(); // Skip `\`
-                        }
-                        't' => {
-                            processed_string.push('\t');
-                            self.advance(); // Skip `\`
-                        }
-                        _ => {
-                            processed_string.push(next_char);
-                            self.advance(); // Skip `\`
-                        }
-                    }
-                }
-            } else {
-                processed_string.push(self.get_current_char());
-            }
-            self.advance();
-        }
-        self.advance(); // consume the closing quote
         Ok(Some(lexeme(
             self.source[self.start..self.current].iter().collect(),
             Token::Literal(LiteralToken::String(symbol::StringLiteral::Regular(
