094a6664178f4bfc186e9deab8363bd7031fe1b5
anishh747 <anishtimalsina747@gmail.com>
2025-05-05 19:06:34 +0545

    feat(parser): handle multiple substitutions in template literal

--- crates/jsompiler_parser/src/template_literal.rs
+++ crates/jsompiler_parser/src/template_literal.rs
@@ -1,6 +1,6 @@
-use super::{expression::Expression, Parser, Statement};
-use crate::{expression, Error, ErrorKind};
-use jsompiler_lexer::symbol::{DelimiterToken, KeywordToken, Token};
+use super::{expression::Expression, Parser};
+use crate::{Error, ErrorKind};
+use jsompiler_lexer::symbol::{DelimiterToken, Token};
 
 #[derive(Debug, Clone)]
 pub enum TemplateLiteral {
@@ -23,7 +23,7 @@ pub struct TemplateSpan {
 #[derive(Debug, Clone)]
 pub struct TemplateMiddleList {
     pub expression: Expression,
-    pub template_middle: String,
+    pub template_middle: Expression,
     pub template_middle_list: Option<Box<TemplateMiddleList>>,
 }
 
@@ -39,68 +39,124 @@ impl Parser {
         }
         let template_head = self.expression()?;
 
-        while self.peek().token != Token::Delimiter(DelimiterToken::Tilde) {
-            if self.peek().token == Token::Identifier('$'.to_string()) {
-                self.advance(); // consume '$'
-                if !self.match_token(&Token::Delimiter(DelimiterToken::OpenBrace)) {
-                    return Err(vec![Error {
-                        error_kind: ErrorKind::UnexpectedToken,
-                        message: "Expected {".to_string(),
-                        line_number: 1,
-                        pos: 2,
-                    }]);
-                }
-
-                let expression = self.expression()?;
-
-                if !self.match_token(&Token::Delimiter(DelimiterToken::CloseBrace)) {
-                    return Err(vec![Error {
-                        error_kind: ErrorKind::UnexpectedToken,
-                        message: "Expected }".to_string(),
-                        line_number: 1,
-                        pos: 2,
-                    }]);
-                }
-                let template_tail = self.expression()?;
-
-                if self.peek().token == Token::Delimiter(DelimiterToken::Tilde) {
-                    self.advance(); // consume '`'
-                    return Ok(Expression::TemplateLiteral(Box::new(
-                        TemplateLiteral::SubstitutionTemplate {
-                            template_head,
-                            expression,
-                            template_spans: TemplateSpan {
-                                template_tail,
-                                template_middle_list: None,
-                            },
-                        },
-                    )));
-                } else {
-                    return Err(vec![Error {
-                        error_kind: ErrorKind::UnexpectedToken,
-                        message: "Expected `".to_string(),
-                        line_number: 1,
-                        pos: 2,
-                    }]);
-                }
-            }
-            self.advance();
-        }
-
         if self.peek().token == Token::Delimiter(DelimiterToken::Tilde) {
-            self.advance(); // consume '`'
-            Ok(Expression::TemplateLiteral(Box::new(
+            self.advance(); // consume closing '`'
+            return Ok(Expression::TemplateLiteral(Box::new(
                 TemplateLiteral::NonSubstitutionTemplate {
                     text: template_head,
                 },
-            )))
-        } else {
-            return Err(vec![Error {
-                error_kind: ErrorKind::UnexpectedToken,
-                message: "Expected `".to_string(),
-                line_number: 1,
-                pos: 2,
-            }]);
+            )));
         }
+
+        // Handle templates with substitutions
+        if self.peek().token == Token::Identifier('$'.to_string()) {
+            self.advance(); // consume '$'
+
+            if !self.match_token(&Token::Delimiter(DelimiterToken::OpenBrace)) {
+                return Err(vec![Error {
+                    error_kind: ErrorKind::UnexpectedToken,
+                    message: "Expected {".to_string(),
+                    line_number: 1,
+                    pos: 2,
+                }]);
+            }
+
+            let first_expression = self.expression()?;
+
+            if !self.match_token(&Token::Delimiter(DelimiterToken::CloseBrace)) {
+                return Err(vec![Error {
+                    error_kind: ErrorKind::UnexpectedToken,
+                    message: "Expected }".to_string(),
+                    line_number: 1,
+                    pos: 2,
+                }]);
+            }
+
+            // Parse the template spans (potentially multiple)
+            let template_spans = self.parse_template_spans()?;
+
+            if !self.match_token(&Token::Delimiter(DelimiterToken::Tilde)) {
+                return Err(vec![Error {
+                    error_kind: ErrorKind::UnexpectedToken,
+                    message: "Expected `".to_string(),
+                    line_number: 1,
+                    pos: 2,
+                }]);
+            }
+
+            return Ok(Expression::TemplateLiteral(Box::new(
+                TemplateLiteral::SubstitutionTemplate {
+                    template_head,
+                    expression: first_expression,
+                    template_spans,
+                },
+            )));
+        }
+
+        Err(vec![Error {
+            error_kind: ErrorKind::UnexpectedToken,
+            message: "Expected $ or `".to_string(),
+            line_number: 1,
+            pos: 2,
+        }])
+    }
+
+    fn parse_template_spans(&mut self) -> Result<TemplateSpan, Vec<Error>> {
+        let template_tail = self.expression()?;
+
+        // No more substitutions
+        if self.peek().token == Token::Delimiter(DelimiterToken::Tilde) {
+            return Ok(TemplateSpan {
+                template_tail,
+                template_middle_list: None,
+            });
+        }
+
+        // We have another substitution
+        if self.peek().token == Token::Identifier('$'.to_string()) {
+            self.advance(); // consume '$'
+
+            if !self.match_token(&Token::Delimiter(DelimiterToken::OpenBrace)) {
+                return Err(vec![Error {
+                    error_kind: ErrorKind::UnexpectedToken,
+                    message: "Expected {".to_string(),
+                    line_number: 1,
+                    pos: 2,
+                }]);
+            }
+
+            let expression = self.expression()?;
+
+            if !self.match_token(&Token::Delimiter(DelimiterToken::CloseBrace)) {
+                return Err(vec![Error {
+                    error_kind: ErrorKind::UnexpectedToken,
+                    message: "Expected }".to_string(),
+                    line_number: 1,
+                    pos: 2,
+                }]);
+            }
+
+            // Recursively parse the rest of the template
+            let next_spans = self.parse_template_spans()?;
+
+            let template_middle_list = TemplateMiddleList {
+                expression,
+                template_middle: template_tail.clone(),
+                template_middle_list: next_spans.template_middle_list.map(Box::new),
+            };
+
+            return Ok(TemplateSpan {
+                template_tail: next_spans.template_tail,
+                template_middle_list: Some(template_middle_list),
+            });
+        }
+
+        Err(vec![Error {
+            error_kind: ErrorKind::UnexpectedToken,
+            message: "Internal parser error in template spans".to_string(),
+            line_number: 1,
+            pos: 2,
+        }])
     }
 }
+
